7781d052cc23feafa70b4be98001f94c21d5407a
Paul Neulinger <paul.neulinger@tanwald.net>
2023-06-19 14:55:02 +0200

    Minikube restart;

--- plots/terra/local/kubernetes.sh
+++ plots/terra/local/kubernetes.sh
@@ -5,15 +5,20 @@
 ########################################################################################################################
 
 function sc_kubernetes_local_up() {
-    minikube start --profile "$_ST_CONTEXT_KUBERNETES_LOCAL" \
-        --namespace serenditree \
-        --driver kvm2 \
-        --kubernetes-version v1.27.0-rc.0 \
-        --cpus 8 \
-        --memory 32000 \
-        --disk-size 32000 \
-        --addons metrics-server \
-        --addons dashboard 2>&1 | sed '/#8426/d'
+    if minikube profile list | grep -q "$_ST_CONTEXT_KUBERNETES_LOCAL"; then
+        minikube profile "$_ST_CONTEXT_KUBERNETES_LOCAL"
+        minikube start
+    else
+        minikube start --profile "$_ST_CONTEXT_KUBERNETES_LOCAL" \
+            --namespace serenditree \
+            --driver kvm2 \
+            --kubernetes-version v1.27.0-rc.0 \
+            --cpus 8 \
+            --memory 32000 \
+            --disk-size 32000 \
+            --addons metrics-server \
+            --addons dashboard 2>&1 | sed '/#8426/d'
+    fi
 
     local -r _ip="$(minikube ip --profile "$_ST_CONTEXT_KUBERNETES_LOCAL")"
     sudo sed -i '/serenditree.io/d' /etc/hosts
