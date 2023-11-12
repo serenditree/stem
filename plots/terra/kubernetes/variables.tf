64e68504b9be56e1a6de7c38f9c0df5b20f9d337
Paul Neulinger <paul.neulinger@tanwald.net>
2023-11-12 11:18:22 +0100

    Kubernetes version update;

--- plots/terra/kubernetes/variables.tf
+++ plots/terra/kubernetes/variables.tf
@@ -28,7 +28,7 @@ variable "zone" {
 variable "kubernetes_version" {
   description = "Kubernetes version."
   type        = string
-  default     = "1.27.4"
+  default     = "1.27.7"
 }
 
 variable "service_level" {
