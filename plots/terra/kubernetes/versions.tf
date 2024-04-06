c665e94c988fb7e1982be23283277850498d9c58
Paul Neulinger <paul.neulinger@tanwald.net>
2024-04-06 13:52:44 +0200

    Version update;

--- plots/terra/kubernetes/versions.tf
+++ plots/terra/kubernetes/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     exoscale = {
       source  = "exoscale/exoscale"
-      version = ">= 0.53.1"
+      version = ">= 0.56.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.0"
+      version = ">= 3.2.2"
     }
   }
 }
