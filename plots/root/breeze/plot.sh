aa4f4ec3d37671e50ff2dbba1f1444ceb273f812
Paul Neulinger <paul.neulinger@tanwald.net>
2024-07-20 15:28:29 +0200

    EFK enabled in podman-compose stack;

--- plots/root/breeze/plot.sh
+++ plots/root/breeze/plot.sh
@@ -20,10 +20,13 @@ if [[ " $* " =~ " build " ]]; then
     [[ -n "$_ARG_DRYRUN" ]] && exit 0
     _CONTAINER_REF=$(buildah from $_ST_FROM_ROOT_BREEZE)
 
-    buildah run --user 0:0 $_CONTAINER_REF -- gem install fluent-plugin-elasticsearch --no-document
+    sc_heading 2 "Installing opensearch plugin..."
+    buildah run --user 0:0 $_CONTAINER_REF -- gem install fluent-plugin-opensearch --no-document
+    sc_heading 2 "Installing fluentd plugin..."
     buildah run --user 0:0 $_CONTAINER_REF -- gem install fluent-plugin-input-gelf --no-document
+    sc_heading 2 "Setting uid/gid..."
     buildah config --user 1001:0 $_CONTAINER_REF
-
+    sc_heading 2 "Adding config..."
     buildah add --chown 1001:0 $_CONTAINER_REF ./fluentd.conf /opt/bitnami/fluentd/conf/
 
     sc_image_config_commit "$_SERVICE" "$_IMAGE" "$_VERSION" "$_TAG" "$_ORDINAL" "$_CONTAINER_REF"
