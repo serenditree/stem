5cc8ac70a98f3707d1a6f203253842f01efbaac7
Paul Neulinger <paul.neulinger@tanwald.net>
2024-09-08 19:36:49 +0200

    Moved script to resources;

---
+++ plots/terra/charts/tekton/resources/finally.sh
@@ -0,0 +1,14 @@
+echo "Aggregated status $(params.PIPELINE_AGGREGATE_STATUS)"
+
+TEXT="Pipeline $(params.PIPELINE_NAME) for revision $(params.PIPELINE_REVISION)"
+STATUS=succeeded
+for _status in $@; do
+    if [[ "${_status##*:}" != "Succeeded" ]]; then
+      STATUS=failed
+    fi
+    _status=${_status/Succeeded/ :green_heart:}
+    _status=${_status/Failed/ :broken_heart:}
+    DETAILS="${DETAILS}\n- ${_status/None/ :x:}"
+done
+
+curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"*${TEXT} ${STATUS}*${DETAILS}\"}" "${WEBHOOK}"
