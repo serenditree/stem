659ff17337159ec3b5fe93df09d5eeaea3338a5b
Paul Neulinger <paul.neulinger@tanwald.net>
2024-09-11 18:55:06 +0200

    Moved duration to title;

--- plots/terra/charts/tekton/resources/finally.sh
+++ plots/terra/charts/tekton/resources/finally.sh
@@ -3,13 +3,13 @@ MINUTES=$((DURATION / 60))
 SECONDS=$((DURATION % 60))
 
 STATUS=$(echo $(params.status) | tr '[:upper:]' '[:lower:]')
-TITLE="*Pipeline \`$(params.pipeline)\` for commit \`$(params.git-sha)\` ${STATUS}:*"
+TITLE="*Pipeline \`$(params.pipeline)\` for commit \`$(params.git-sha)\` ${STATUS} in ${MINUTES}m ${SECONDS}s:*"
 for _status in $@; do
     _status=${_status/Succeeded/ :green_heart:}
     _status=${_status/Failed/ :broken_heart:}
     DETAILS="${DETAILS}\n- ${_status/None/ :x:}"
 done
-DETAILS="${DETAILS}\n\n$(params.image-sha)\n\nDuration: ${MINUTES}m${SECONDS}s"
+DETAILS="${DETAILS}\n\n$(params.image-sha)"
 
 echo -e "${TITLE}\n${DETAILS}\n"
 
