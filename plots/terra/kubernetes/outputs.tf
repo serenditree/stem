f1c6d838cac0fd75bcd4c43d8f27808079bef0d9
Paul Neulinger <paul.neulinger@tanwald.net>
2024-09-29 23:01:19 +0200

    Output filenames only;

--- plots/terra/kubernetes/outputs.tf
+++ plots/terra/kubernetes/outputs.tf
@@ -1,23 +1,22 @@
 ########################################################################################################################
 # Outputs
 ########################################################################################################################
-output "serenditree_scaler_key" {
-  value = exoscale_iam_api_key.serenditree_scaler.key
-}
-
-output "serenditree_scaler_secret" {
-  value     = exoscale_iam_api_key.serenditree_scaler.secret
-  sensitive = true
-}
-
 output "serenditree_scaler_file" {
   value = local_sensitive_file.serenditree_scaler.filename
 }
 
-output "serenditree_sks_endpoint" {
-  value = exoscale_sks_cluster.serenditree.endpoint
+output "serenditree_data_file" {
+  value = local_sensitive_file.serenditree_data.filename
+}
+
+output "serenditree_backup_file" {
+  value = local_sensitive_file.serenditree_backup.filename
 }
 
 output "serenditree_sks_kubeconfig" {
   value = local_sensitive_file.serenditree_sks_kubeconfig_file.filename
+}
+
+output "serenditree_sks_endpoint" {
+  value = exoscale_sks_cluster.serenditree.endpoint
 }
