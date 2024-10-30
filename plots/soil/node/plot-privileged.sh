feaa9dbd4c82eb388fabec619c94c4b593ed1c28
Paul Neulinger <paul.neulinger@tanwald.net>
2024-10-30 21:11:02 +0100

    Introduced privileged plot;

---
+++ plots/soil/node/plot-privileged.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Prepares node repository for the installation of a specific node version.
+# $1 Current node plot.
+function sc_plot_privileged() {
+    if [[ "$1" == "plot-node.sh:base:0" ]]; then
+        sc_heading 1 "Preparing node repository"
+        trap 'sudo rm -rf /etc/yum.repos.d/nodesource-nodejs.repo' EXIT
+        curl -fsSL "https://rpm.nodesource.com/setup_${_ST_VERSION_NODE}" | sudo bash -
+    fi
+}
