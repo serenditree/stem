0815e157ae282c58451e0af306fa3d4114ee780c
Paul Neulinger <paul.neulinger@tanwald.net>
2026-07-13 15:26:22 +0200

    Fixed checkout by sha;

--- charts/terra/tekton/resources/git.sh
+++ charts/terra/tekton/resources/git.sh
@@ -1,8 +1,7 @@
-set -o errexit
-date +%s > $(results.start.path)
+date +%s > "$(results.start.path)"
 
-mkdir -pv $(params.subdirectory)
-cd $(params.subdirectory)
+mkdir -pv "$(params.subdirectory)"
+cd "$(params.subdirectory)" || exit 1
 
 if [[ -d .git ]] && [[ -n "$(params.clone)" ]]; then
   echo "Cleaning repository..."
@@ -10,10 +9,15 @@ if [[ -d .git ]] && [[ -n "$(params.clone)" ]]; then
 fi
 
 if [[ ! -d .git ]]; then
-  git clone $(params.url) .
+  git clone "$(params.url)" .
+else
+  git fetch --all
 fi
 
-git checkout $(params.revision)
-git pull --ff-only
+if git show-ref --verify --quiet "refs/remotes/origin/$(params.revision)"; then
+  git checkout -B "$(params.revision)" "origin/$(params.revision)"
+else
+  git checkout "$(params.revision)"
+fi
 
-git log -1 --pretty="%h" | tr -d '\n' | tee $(results.sha.path)
+git log -1 --pretty="%h" | tr -d '\n' | tee "$(results.sha.path)"
