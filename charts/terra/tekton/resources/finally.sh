9b7711a3017a8d73f91b37cbd97bd87ecc8e6a4b
Paul Neulinger <paul.neulinger@tanwald.net>
2026-07-13 18:27:25 +0200

    Swapped status text and icon;

--- charts/terra/tekton/resources/finally.sh
+++ charts/terra/tekton/resources/finally.sh
@@ -5,9 +5,10 @@ SECONDS=$((DURATION % 60))
 STATUS=$(echo $(params.status) | tr '[:upper:]' '[:lower:]')
 TITLE="*Pipeline \`$(params.pipeline)\` for commit \`$(params.git-sha)\` ${STATUS} in ${MINUTES}m ${SECONDS}s:*"
 for _status in $@; do
-    _status=${_status/Succeeded/ :green_heart:}
-    _status=${_status/Failed/ :broken_heart:}
-    DETAILS="${DETAILS}\n- ${_status/None/ :x:}"
+    _status="${_status#*:} ${_status%:*}"
+    _status=${_status/Succeeded/:green_heart:}
+    _status=${_status/Failed/:broken_heart:}
+    DETAILS="${DETAILS}\n${_status/None/:x:}"
 done
 DETAILS="${DETAILS}\n\n$(params.image-sha)"
 
